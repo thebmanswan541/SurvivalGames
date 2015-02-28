3f46547d4008c0079c7e653f3a5bbf2267d5c860
BuildTools <unconfigured@null.spigotmc.org>
2015-02-27 23:11:40 -0500

    Initial commit

---
+++ src/main/java/me/thebmanswan541/SurvivalGames/SurvivalGames.java
@@ -0,0 +1,13 @@
+package me.thebmanswan541.SurvivalGames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Really good class
+ * I'm so fucking cool
+ * swag swag
+ * yolo
+ * jdkgjsd
+ */
+public class SurvivalGames extends JavaPlugin{
+}
